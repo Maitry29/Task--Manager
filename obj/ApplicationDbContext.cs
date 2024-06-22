f8b491f146de35ac8c6fd42b2c57462ff5b25d37
Maitry29 <118123849+Maitry29@users.noreply.github.com>
2024-06-22 17:16:37 +0530

    Add files via upload

---
+++ obj/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task_Manager.Models;
+using Task = Task_Manager.Models.Task;
+using System.Collections.Generic;
+
+namespace Task_Manager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) 
+        {
+        
+        }
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>().HasData(
+
+                new Task { Id = 1, Title = "Meeting", Description = "Important Office meeting on monday 11 AM." },
+
+                new Task { Id = 2, Title = "Birthday", Description = "Mom's birthday on saturday." }
+
+                );
+        }
+    }
+}
